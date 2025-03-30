1d6e328a575283a5a3961c8ce131304813d192eb
elenaPadala <elena.padala@gmail.com>
2025-03-30 15:29:45 -0700

    add everything

---
+++ scripts.js
@@ -0,0 +1,31 @@
+/* --- FUNCTION FOR THE CLOCK --- */
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function checkDate() {
+    var today = new Date();
+    var day = today.getDate().toString().padStart(2, "0");
+    var month = (today.getMonth() + 1).toString().padStart(2, "0");
+    var year = today.getFullYear();
+    $("#date").html(month + "-" + day + "-" + year);
+}
+
+$(document).ready(function() {
+    checkDate();
+});
+
+
